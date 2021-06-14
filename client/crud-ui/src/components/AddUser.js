4daa67c48fd13de8d301d07007b03075c067f327
Rajan Patel <rp581988@gmail.com>
2021-06-14 13:03:18 +0530

    deleted the client app

--- client/crud-ui/src/components/AddUser.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const AddUser = () => {
-    return (
-        <div>
-            <h1>Add User</h1>
-        </div>
-    )
-}
--- client/crud-ui/src/components/UserDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const UserDetails = () => {
-    return (
-        <div>
-            <h1>User Details</h1>
-        </div>
-    )
-}
